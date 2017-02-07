3098609007f2d3667c3f64c5f35f7a45931a9393
dunmait <dunmait@stealth.org>
2017-02-07 15:06:01 -0200

    mensagem 2

--- TestGit.java
+++ TestGit.java
@@ -2,5 +2,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Exemplo de teste de uso do git");
 		System.out.println("Trabalhando com branches");
+		System.out.println("Mensagem 2");
 	}
 }
